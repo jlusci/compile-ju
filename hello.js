7c1d8cf1f2c9d985718b22ddad6f74929340b2b2
jlusci <jlusci@gmail.com>
2014-07-30 17:06:53 -0700

    added list indexing to hello.js

--- hello.js
+++ hello.js
@@ -6,18 +6,24 @@ function main() {
 	print x+5*9;
 	var x = x+3;
 	print x;
-	var y = [1,2];
+	var y = [7,9,600,78394];
 	print y;
-//	y[0];
-	var dict = {'key':'value'};
-	print dict;
-//	dict['key'];
+	print y[2]+y[0];
+	// z = y[0];
+	// print z;
+	var d = {'key':'value', 'my dictionary': 'my value'};
+	print d;
+	var this = {'key': 1};
+	print this;
+	print this['key'];
+	d['key'];
+	print d['my dictionary'];
 	if (x % 5 == 0){
 		print "yes, x mod 5 is zero";
 	} else {
 		print "no, x mod 5 is not zero";
 	}
-	for (var i = 0; i <= 2; i = i + 1) {
+	for (var i = 0; i >= -5; i = i - 1) {
 		print i;
 	}
 	while (x < 10){
